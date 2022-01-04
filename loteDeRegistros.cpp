6942312a802cb955c1a130fcaf83a7ae328bddfd
Manuel Firman <92244840+manuelfirman@users.noreply.github.com>
2022-01-03 23:30:48 -0300

    Update and rename main.cpp to loteDeRegistros.cpp

--- loteDeRegistros.cpp
+++ loteDeRegistros.cpp
@@ -2,6 +2,30 @@
 
 using namespace std;
 
+/*
+Se dispone de un lote de 300 registros (uno por artículo), cada uno de los cuales tiene la
+siguiente información:
+- Número de artículo (4 dígitos no correlativos).
+- Cantidad de unidades en stock.
+- Precio Unitario.
+El lote está desordenado.
+Se dispone de otro lote de registros, cada uno de los cuales corresponde a una venta con la siguiente
+información:
+- Número de cliente (1 a 300).
+- Número de artículo (4 dígitos, no correlativos).
+- Cantidad de unidades vendidas.
+El último registro de este lote tiene número de cliente cero y no debe procesarse. Desarrollar el
+programa que determine e imprima:
+a) Un listado de las ventas realizadas, con el siguiente formato:
+Listado de ventas
+Nro. Cliente    Nro. Artículo   Cantidad Vendida    Importe Total
+    99              99                  999             999.99
+b) Informar cuál es el número de cliente que más compró en total (en pesos).
+c) Informar cada uno de los números de los artículos que no hayan registrado ventas.
+
+NOTA: se utilizan arrays y ciclos de 5 para agilizar la carga y prueba del programa.
+*/
+
 int main()
 {
     //Vectores
